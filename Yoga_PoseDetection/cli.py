7bb555ab48278efc2058bf3015a9696db898fa9f
Nibosi <k22137kk@aitech.ac.jp>
2024-05-30 13:12:55 +0900

    init: mediapipe python 初期構成

--- Yoga_PoseDetection/cli.py
+++ Yoga_PoseDetection/cli.py
@@ -1,9 +1,30 @@
-from .mylib.pose_detection import Pose_Detection
+from .mylib.Pose_Landmarks import PoseLandmarks as Pose_Detection
+import cv2 as cv
 
 def main() -> None:
     print("Hello from cli.py")
-    Pose_Detection.hello()
-    Pose_Detection._print_version()
+    Pose_Landmark = Pose_Detection()
+    # カメラキャプチャを開始
+    cap = cv.VideoCapture(0)
+    print("Capture Size ------------------------------")
+    print("width: ", cap.get(cv.CAP_PROP_FRAME_WIDTH))
+    print("height: ", cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+    print("-------------------------------------------")
+
+    while True:
+        ret, image = cap.read()
+        if not ret:
+            break
+        # フレームを処理して結果画像を取得
+        resultImage = Pose_Landmark.main(image)
+        # 結果画像を表示
+        cv.imshow('MediaPipe Pose Demo', resultImage)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    # キャプチャを解放してウィンドウを破棄
+    cap.release()
+    cv.destroyAllWindows()
 
 if __name__ == '__main__':
     print("Debug directly from __main__.py")
