639ec262fec7fdeb188ce21df0e1c179d637a1e5
otakumi-bekon <otatakumi2004@gmail.com>
2024-06-27 17:54:18 +0900

    シーン移動とゴール判定です

---
+++ YogaFighter/Assets/Seen script/Goal.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    [SerializeField] private int goal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(set_segment.top_position.y);
+        Debug.Log("GG");
+        if(set_segment.top_position.y >= goal) {
+            // Debug.Log("これ"+set_segment.top_position.y);
+            Debug.Log("hogehoge");
+            SceneManager.LoadScene("Goal Seen");
+        }
+    }
+}
