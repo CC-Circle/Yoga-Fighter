ddd20e89cf80b122078c36f62b084a4406f16585
otakumi-bekon <otatakumi2004@gmail.com>
2024-06-27 19:13:54 +0900

    ぷるりくだっぴ

--- YogaFighter/Assets/Seen script/Goal.cs
+++ YogaFighter/Assets/Seen script/Goal.cs
@@ -9,18 +9,21 @@ public class Goal : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("プレイ画面です");
         
     }
 
     // Update is called once per frame
     void Update()
     {
+
         
         //Debug.Log(set_segment.top_position.y);
         if(set_segment.top_position.y >= goal) {
             // Debug.Log("これ"+set_segment.top_position.y);
-            // Debug.Log("hogehoge");
-            SceneManager.LoadScene("Goal");
+            Debug.Log("Goalです");
+            set_segment.top_position = new Vector3(0.0f, 0.0f, 0.0f);
+            // SceneManager.LoadScene("Goal");
         }
     }
 }
--- YogaFighter/Assets/Seen script/Title.cs
+++ YogaFighter/Assets/Seen script/Title.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class Title : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         Debug.Log("ゴール画面です。");
     }
